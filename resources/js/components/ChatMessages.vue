de1234d1c16f9a7aa5e032fc7de24ac350316372
DaWeird <waihongdc@gmail.com>
2022-12-01 17:41:52 +0800

    Initial commit

---
+++ resources/js/components/ChatMessages.vue
@@ -0,0 +1,32 @@
+// resources/assets/js/components/ChatMessages.vue
+
+<template>
+  <ul>
+    <li class="left clearfix" v-for="message in messages" :key="message.id">
+      <div class="clearfix">
+        <div class="header">
+          <strong>
+            {{ message.user.name }}
+          </strong>
+    <span>{{ new Date(message.created_at).toLocaleString() }}</span>
+
+        </div>
+        <p>
+          {{ message.message }}
+        </p>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["messages"],
+  computed: {
+    date() {
+      const created_at = getStatusDate(); // returns string 
+      return dayjs(date, 'YYYY-MM-DD HH:mm')
+    }
+  }
+};
+</script>
